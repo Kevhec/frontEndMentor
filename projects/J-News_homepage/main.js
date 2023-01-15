79102e0254e18baa04f6f9c340182dc062d0f0d7
Kevhec <kevinalejandrohenca@gmail.com>
2023-01-14 19:46:59 -0500

    new project check for menu

---
+++ projects/J-News_homepage/main.js
@@ -0,0 +1,13 @@
+const navButton = document.querySelector('.nav-button');
+const navHamb = document.querySelector('.nav-hamb');
+const mainNav = document.querySelector('.main-nav');
+const navOverlay = document.querySelector('.background-overlay');
+
+navButton.addEventListener('click', toggleEvents);
+navOverlay.addEventListener('click', toggleEvents);
+
+function toggleEvents() {
+  navHamb.classList.toggle('active');
+  mainNav.classList.toggle('active');
+  navOverlay.classList.toggle('active');
+}
